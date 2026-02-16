a7510013895de1641ac3be3ddd94ad415aa4efdb
Johlan Pretorius <cheezzz@icloud.com>
2026-02-16 10:46:35 +0200

    Added zsh as shell in bootstrap script, set it as johlan's default shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -100,7 +100,7 @@ msg "Installing base system with pacstrap"
 
 pacstrap -K "${MOUNT}" \
     base base-devel linux linux-headers linux-firmware \
-    intel-ucode btrfs-progs ansible python-passlib git
+    intel-ucode btrfs-progs ansible python-passlib git zsh
 
 # --- Step 7: Generate fstab ---
 msg "Generating fstab"
